379728a2e494afd337d7a7a4480acf04a2716fef
Tamang-Suvam <90154468+Tamang-Suvam@users.noreply.github.com>
2022-08-14 11:03:20 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (24.11%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -17,30 +17,55 @@ class Solution {
     public TreeNode bstFromPreorder(int[] preorder) {
          TreeNode root = new TreeNode(preorder[0]);
 
-         for(int i=1; i<preorder.length; i++){
-             TreeNode temp = root;
-             while(true){
-                 if(preorder[i] < temp.val){
-                     if(temp.left != null && temp.val > preorder[i]){
-                         temp = temp.left;
-                     }
-                     else{
-                         temp.left = new TreeNode(preorder[i]);
-                         break;
-                     }
-                 }
-                 if(preorder[i] > temp.val){
-                     if(temp.right != null && temp.val < preorder[i]){
-                         temp = temp.right;
-                     }
-                     else{
-                         temp.right = new TreeNode(preorder[i]);
-                         break;
-                     }
-                 }
-             }
-         }
+         // for(int i=1; i<preorder.length; i++){
+         //     TreeNode temp = root;
+         //     while(true){
+         //         if(preorder[i] < temp.val){
+         //             if(temp.left != null && temp.val > preorder[i]){
+         //                 temp = temp.left;
+         //             }
+         //             else{
+         //                 temp.left = new TreeNode(preorder[i]);
+         //                 break;
+         //             }
+         //         }
+         //         if(preorder[i] > temp.val){
+         //             if(temp.right != null && temp.val < preorder[i]){
+         //                 temp = temp.right;
+         //             }
+         //             else{
+         //                 temp.right = new TreeNode(preorder[i]);
+         //                 break;
+         //             }
+         //         }
+         //     }
+         // }
         
+        for(int i=1; i<preorder.length;i++){
+            helperCreate(root, preorder[i]);
+        }
         return root;
     }
+    
+    public void helperCreate(TreeNode root, int value){
+        TreeNode temp = root;
+      
+        if(value < temp.val){
+            if(temp.left != null){
+                helperCreate(temp.left, value);
+            }
+            else{
+                temp.left = new TreeNode(value);
+            }
+        }
+        if(value > temp.val){
+            if(temp.right != null && temp.val < value){
+                helperCreate(temp.right, value);
+            }
+            else{
+                temp.right = new TreeNode(value);
+            }
+        }
+    }
+    
 }
