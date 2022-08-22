7b671639cc53d660b0f5e1fcb429bd61c8626270
Tamang-Suvam <90154468+Tamang-Suvam@users.noreply.github.com>
2022-08-22 15:56:54 +0530

    Time: 1 ms (100.00%), Space: 50.2 MB (75.95%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,18 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        ArrayList <Integer> helper = new ArrayList<>();
-        if(nums.length == 1){
-            return nums[0];
-        }
-        helper.add(nums[0]);
-        for(int i=1;i<nums.length;i++){
-            if(helper.contains(nums[i])){
-                helper.remove(Integer.valueOf(nums[i]));
+        int result = 0;
+        for (int i = 0; i<nums.length; i++)
+            {
+                result ^= nums[i];
             }
-            else{
-                helper.add(nums[i]);
-            }
-        }
-        return helper.get(0);
+	    return result;
     }
 }
