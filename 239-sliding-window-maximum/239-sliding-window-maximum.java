8385f242706c56327905923c71652e2feefed4de
Tamang-Suvam <90154468+Tamang-Suvam@users.noreply.github.com>
2022-08-28 20:57:01 +0530

    Time: 58 ms (57.95%), Space: 150.2 MB (51.05%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -3,18 +3,6 @@ class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int j = 0;
         int [] result = new int [nums.length-k+1];
-//         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-//         for(int i = 0; i < k; i++)
-//             pq.add(nums[i]);
-        
-//         for(int i = k; i < nums.length; i++){
-//             int maxElement = pq.peek();
-//             result[i-k] = maxElement;
-//             pq.remove(nums[i-k]);
-//             pq.add(nums[i]);
-//         }
-
-//        result[nums.length-k] = pq.peek();
         if(nums.length==1)
             return nums;
         Deque<Integer> Qi = new LinkedList<Integer>();
